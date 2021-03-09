5b7554c3c7d2095a3a03a62334493d98a725f209
Feetu Nyrhinen <feetu.nyrhinen@gmail.com>
2021-03-10 00:20:30 +0100

    Use Apollo to fetch data from the GraphQL server

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,27 @@
 </template>
 
 <script>
+import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+import { DefaultApolloClient } from "@vue/apollo-composable";
+import { provide, defineComponent } from "vue";
 import TemperatureView from "./views/TemperatureView.vue";
 import TemperatureTable from "./components/TemperatureTable.vue";
-import { defineComponent } from "vue";
+
+const client = new ApolloClient({
+  uri: "http://37.120.176.60:4000/",
+  cache: new InMemoryCache(),
+});
 
 export default defineComponent({
   name: "App",
   components: { TemperatureView, TemperatureTable },
+  setup() {
+    provide(DefaultApolloClient, client);
+  },
   data() {
     return {
-      currentDate: new Date()
-    }
+      currentDate: new Date(),
+    };
   },
 });
 </script>
